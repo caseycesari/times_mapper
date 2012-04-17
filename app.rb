ff5954b149c9eea252f304692d22e37c25309500
Casey Thomas <cpthomas@gmail.com>
2012-04-17 19:29:27 -0400

    Readded compass config line

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ require 'date'
 configure do
   set :haml, {:format => :html5, :escape_html => true}
   set :scss, {:style => :compact, :debug_info => false}
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, '/views/config.rb'))
 end
 
 get '/stylesheets/:name.css' do
