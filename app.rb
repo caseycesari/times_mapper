d7876386ab72792d39ad8a729ba735d141fc6bb4
Casey Thomas <cpthomas@gmail.com>
2012-04-17 18:12:45 -0400

    mend
    Adding sass config file, index template, and scss files from TimesGrapher

--- app.rb
+++ app.rb
@@ -7,6 +7,19 @@ require 'json'
 require 'net/http'
 require 'date'
 
+configure do
+  set :haml, {:format => :html5, :escape_html => true}
+  set :scss, {:style => :compact, :debug_info => false}
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss(:"stylesheets/#{params[:name]}" )
+end
+
 get '/' do
   'Hello World!'
+
+  haml:index
 end
