074d26e10c1c800b754046da9bc935941a07767d
Casey Thomas <cpthomas@gmail.com>
2012-04-17 19:15:15 -0400

    Removed Compass configuration line

--- app.rb
+++ app.rb
@@ -10,7 +10,6 @@ require 'date'
 configure do
   set :haml, {:format => :html5, :escape_html => true}
   set :scss, {:style => :compact, :debug_info => false}
-  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
 end
 
 get '/stylesheets/:name.css' do
