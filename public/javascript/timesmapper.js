438bb38d4ccfd160eaa846640ab30ad9d0aa49c5
Casey Thomas <cpthomas@gmail.com>
2012-04-18 11:50:48 -0400

    Added functions to for pushstate handling. Added some basic comments

--- public/javascript/timesmapper.js
+++ public/javascript/timesmapper.js
@@ -2,6 +2,7 @@ if (typeof TM === 'undefined' || !TM) {
   var TM = {};
 }
 
+// Map constructer
 TM.Map = function(options) {
   this.options = options;
   this.markers = [];
@@ -15,8 +16,10 @@ TM.Map = function(options) {
   });
 }
 
+// TM.Map utlities
 TM.Map.prototype = {
 
+  // Add a single marker to the map
   addMarker: function(options) {
     var markerLocation = new L.LatLng(options.lat, options.lng);
     var marker = new L.Marker(markerLocation);
@@ -25,16 +28,19 @@ TM.Map.prototype = {
     this.markers.push(marker);
   },
 
+  // Remove all the markers from the map
   removeMarkers: function() {
     for (marker in this.markers) {
       this.removeMarker(this.markers[marker]);
     }
   },
 
+  // Remove a single marker from the map
   removeMarker: function(marker) {
     this.m.removeLayer(marker);
   },
 
+  // Remove and delete all markers
   deleteMarkers: function() {
     for (marker in this.markers) {
       this.removeMarker(this.markers[marker]);
@@ -44,6 +50,50 @@ TM.Map.prototype = {
 
 };
 
+// General utility function namespace
 TM.utils = {
+
+  // Triggered if a user comes to /search/:topic
+  // rather than /. Executes a search for :topic
+  // and fills input with :topic
+  refresh: function(location) {
+    var url;
+
+    url = location.split('/')
+
+    $('.topic').val(url[2]);
+    TM.utils.makeQuery(url[2]);
+  },
+
+  // Update the url with the search topic
+  updateURL: function(topic) {
+    history.pushState(null, '', '/search/' + topic);
+  },
+
+  // Make a query to the NYT Article API
+  // If the query is successful, update the URL
+  // and pass on the results.
+  makeQuery: function(topic) {
+    var cleanTopic;
+
+    cleanTopic = escape(topic);
+
+    $.ajax({
+      url: '/query/' + topic,
+      success: function(data) {
+        TM.utils.updateURL(topic);
+        console.log(data.results);
+        $('.results').html(JSON.stringify(data.results));
+      }
+    })
+  },
+
+  // Resets the page to the default state
+  // Clears map, inputs, etc.
+  clear: function() {
+    $('.topic').val('');
+    history.pushState(null, '', '/');
+    //clear map
+  }
 
 }
