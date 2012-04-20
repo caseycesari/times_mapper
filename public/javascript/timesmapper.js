c1225fffe94d8e793fb89062acd6820e359aa74d
Casey Thomas <cpthomas@gmail.com>
2012-04-20 12:58:58 -0400

    Implemented tooltip functionality, moved functions around, cleaned up code, added comments.

--- public/javascript/timesmapper.js
+++ public/javascript/timesmapper.js
@@ -19,7 +19,7 @@ TM.Map = function(container, options, callback) {
         
       if (typeof callback === "function" && callback) { callback(); }
   });
-}
+};
 
 // TM.Map utlities
 TM.Map.prototype = {
@@ -38,8 +38,10 @@ TM.Map.prototype = {
       group = new L.LayerGroup();
 
     for (location in locations) {
-      var circle = this.createCircleMarker(locations[location]);
-      group.addLayer(circle)
+      if (locations.hasOwnProperty(location)) {
+        var circle = this.createCircleMarker(locations[location]);
+        group.addLayer(circle);
+      }
     }
 
     this.m.addLayer(group);
@@ -59,7 +61,9 @@ TM.Map.prototype = {
     var marker;
 
     for (marker in this.markers) {
-      this.removeMarker(this.markers[marker]);
+      if (this.markers.hasOwnProperty(marker)) {
+        this.removeMarker(this.markers[marker]);
+      }
     }
   },
 
@@ -68,18 +72,25 @@ TM.Map.prototype = {
     this.m.removeLayer(marker);
   },
 
+  // Remove all layers from the map
   removeLayers: function() {
     var layer;
 
     for (layer in this.layers) {
-      this.m.removeLayer(this.layers[layer]);
+      if (this.layers.hasOwnProperty(layer)) {
+        this.m.removeLayer(this.layers[layer]);
+      }
     }
   },
 
   // Remove and delete all markers
   deleteMarkers: function() {
+    var marker; 
+
     for (marker in this.markers) {
-      this.removeMarker(this.markers[marker]);
+      if (this.markers.hasOwnProperty(marker)) {
+        this.removeMarker(this.markers[marker]);
+      }
     }
     this.markers.length = 0;
   }
@@ -92,11 +103,22 @@ TM.cache = {};
 // General utilities namespace
 TM.utils = {
 
+  // Triggered when page url changes
+  handlePopState: function() {
+    var url = location.pathname.split('/');
+
+    if (url[1] && url[1] === 'search') {
+      TM.utils.refresh(location.pathname);
+    } else {
+      TM.utils.clear();
+    }
+  },
+
   // Triggered if a user comes to /search/:topic
   // rather than /!. Executes a search for :topic
   // and fills input with :topic
   refresh: function(location) {
-    var url = location.split('/')
+    var url = location.split('/');
 
     if(url[1] === 'search') {
       $('.topic').val(url[2]);
@@ -107,16 +129,6 @@ TM.utils = {
   // Update the url with the search topic
   updateURL: function(topic) {
     history.pushState(null, '', '/search/' + topic);
-  },
-
-  handlePopState: function() {
-    var url = location.pathname.split('/');
-
-    if (url[1] && url[1] === 'search') {
-      TM.utils.refresh(location.pathname);
-    } else {
-      TM.utils.clear();
-    }
   },
 
   // Creates the initial markers for each state
@@ -132,7 +144,9 @@ TM.utils = {
         var options = {
           fillColor: '#222',
           fillOpacity: 0.5,
-          stroke: false
+          stroke: true,
+          weight: 0,
+          color: '#414141'
         };
     
         var circle = map.createCircleMarker(loc, 0, options);
@@ -140,24 +154,16 @@ TM.utils = {
     });
   },
 
+  // Checks to see if the topic is already cached
+  // or if we need to query the NYT API
   checkForData: function(topic) {
     if (TM.cache[topic]) {
-      console.log('cache');
       TM.utils.processResults(topic);
     } else  {
-      console.log('query');
       TM.cache[topic] = {};
       TM.cache[topic].articles = [];
-      TM.utils.makeQuery(topic, 0)
+      TM.utils.makeQuery(topic, 0);
     }
-  },
-
-  processResults: function(topic) {
-    if ($.isEmptyObject(map.markers)) {
-      TM.utils.addStateMarkers();
-    }
-    TM.cache[topic].states = {};
-    TM.utils.countArticles(topic, TM.cache[topic].articles);
   },
 
   // Make a query to the NYT Article API
@@ -178,54 +184,149 @@ TM.utils = {
     });
   },
 
+  // Makes sure the state markers exists
+  // the moves on to processing the response
+  // from the NYT API
+  processResults: function(topic) {
+    if ($.isEmptyObject(map.markers)) {
+      TM.utils.addStateMarkers();
+    }
+    TM.cache[topic].states = {};
+    TM.utils.countArticles(topic, TM.cache[topic].articles);
+  },
+
   // Count the number of articles per state
   countArticles: function(topic, articles) {
+    var article;
+
     for (article in articles) {
-      var state = TM.utils.findState(articles[article].geo_facet);
-      if (state) {
-        if (TM.cache[topic].states[state]) {
-          TM.cache[topic].states[state]++ 
-        } else {
-          TM.cache[topic].states[state] = 1;
+      if (articles.hasOwnProperty(article)) {
+        var state = TM.utils.findState(articles[article].geo_facet);
+        
+        if (state) {
+          if (TM.cache[topic].states[state]) {
+            TM.cache[topic].states[state]++;
+          } else {
+            TM.cache[topic].states[state] = 1;
+          }
         }
       }
     }
     TM.utils.updateStateMarkers(topic);
   },
 
+  // Look for a US State in the an article's
+  // geo_facet tags
   findState: function(geo_facet) {
     var tag,
       state;
 
     for (tag in geo_facet) {
-      for (state in TM.states) {
-        if (geo_facet[tag] === TM.states[state].name) {
-          return geo_facet[tag];
+      if (geo_facet.hasOwnProperty(tag)) {
+        for (state in TM.states) {
+          if (geo_facet[tag] === TM.states[state].name) {
+            return geo_facet[tag];
+          }
         }
-      }
+      } 
     }
   },
 
   // Update the state marker radius size with
   // the number of articles per state * a factor
   updateStateMarkers: function(topic) {
+    var marker;
     var stateCounts = TM.cache[topic].states;
     
     for (marker in map.markers){
       if (map.markers.hasOwnProperty(marker)) {
         if (TM.cache[topic].states[marker]) {
-          TM.utils.updateMarkerRadius(marker, TM.cache[topic].states[marker] * 75000);
+          var factor = (Math.log(TM.cache[topic].states[marker] + 1) * 100000);
+
+          TM.utils.setRolloverEvents(map.markers[marker], marker, topic);
+          TM.utils.updateMarkerRadius(marker, factor);
         } else {
           TM.utils.updateMarkerRadius(marker, 0);
         }
       }
     }
+  },
 
+  // Set up marker toolips 
+  setRolloverEvents: function (marker, id, topic) {
+    var offset = $('.leaflet-control-container').offset();
+
+    marker.on("mouseover", function () {
+      TM.utils.setMarkerHighlight(marker);
+
+      var html = TM.utils.makeArticleSnippet(id, topic);
+      TM.utils.showToolTip(html);
+
+      $(document).mousemove(function(e){
+        $('.tooltip').css({
+          left: e.pageX - offset.left - 57,
+          top: e.pageY - offset.top - 60
+        });
+      });
+    });
+
+    marker.on("mouseout", function () { 
+      TM.utils.removeMarkerHighlight(marker);
+      $('.tooltip').hide();
+      $(document).unbind('mousemove');
+    });
   },
 
   // Update the given marker's radius
   updateMarkerRadius: function(id,size) {
     map.markers[id].setRadius(size); 
+  },
+
+  // Compile the HTML needed for the marker tooltips
+  makeArticleSnippet: function(id, topic) {
+    var html,
+      title,
+      article;
+
+    title = id.toLowerCase();
+    var articles = (TM.cache[topic].states[id] === 1) ? 'Article' : 'Articles';
+
+    html =  "<h2>" + title + "</h2>" +
+            "<p>" + TM.cache[topic].states[id] + 
+            ' ' + articles + '<p>'; 
+
+    return html;
+  },
+
+  // Set a marker to hightlight style
+  setMarkerHighlight: function(marker) {
+    marker.setStyle({
+      stroke: true,
+      weight: 2,
+      color: '#414141',
+      opacity: 1
+    });
+  },
+
+  // Set's a marker back to the default style
+  removeMarkerHighlight: function(marker) {
+    marker.setStyle({
+      stroke: true,
+      weight: 0
+    });
+  },
+
+  // Creates and shows the tooltip
+  // HTML element
+  showToolTip: function (html) {
+    if($('.tooltip').length) {
+        $('.tooltip').html(html).show();
+    } else {
+      $('<div/>', {
+        class: 'tooltip',
+        html: html
+      }).appendTo('.leaflet-control-container').show();
+    }
   },
 
   // Resets the page to the default state
@@ -237,10 +338,12 @@ TM.utils = {
 
   // Set each marker's radius back to 0
   resetMarkerRadius: function() {
+    var marker;
+
     for (marker in map.markers) {
       if (map.markers.hasOwnProperty(marker)) {
         TM.utils.updateMarkerRadius(marker, 0);
       }
     }
   }
-}
+};
